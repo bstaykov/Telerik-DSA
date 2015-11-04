674c1e61d931ab9bfb1626074e7738f93b62633b
bstaykov <boycho.staykov@gmail.com>
2015-11-04 14:58:33 +0200

    Enqueue

---
+++ 2015/AdvancedDataStructures/AdvancedDataStructures/PriorityQueue.cs
@@ -0,0 +1,70 @@
+﻿namespace AdvancedDataStructures
+{
+    using System;
+
+    public class PriorityQueue<T> where T : IComparable
+    {
+        private const int InitialCapacity = 16;
+        private T[] data;
+        private int capacity;
+        private int count;
+
+        public PriorityQueue()
+            : this(InitialCapacity)
+        {
+        }
+
+        public PriorityQueue(int capacity)
+        {
+            this.capacity = capacity;
+            this.data = new T[capacity];
+            this.count = 0;
+        }
+
+        public void Enqueue(T item)
+        {
+            this.data[this.count] = item;
+
+            if (this.count != 0)
+            {
+                this.CompareWithParrent(this.count);
+            }
+
+            this.count++;
+            if (this.count == this.capacity - 1)
+            {
+                this.IncreaseCapacity();
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Join(", ", this.data);
+        }
+
+        private void CompareWithParrent(int index)
+        {
+            var parrentIndex = (index - 1) / 2;
+            if (this.data[parrentIndex].CompareTo(this.data[index]) == 1)
+            {
+                var tempItem = this.data[parrentIndex];
+                this.data[parrentIndex] = this.data[index];
+                this.data[index] = tempItem;
+
+                if (parrentIndex != 0)
+                {
+                    this.CompareWithParrent(parrentIndex);
+                }
+            }
+        }
+
+        private void IncreaseCapacity()
+        {
+            var newCapacity = this.capacity * 2;
+            var newData = new T[newCapacity];
+            Array.Copy(this.data, newData, this.count);
+            this.data = newData;
+            this.capacity = newCapacity;
+        }
+    }
+}
---
+++ 2015/AdvancedDataStructures/AdvancedDataStructures/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace AdvancedDataStructures
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var priorityQueue = new PriorityQueue<string>();
+            priorityQueue.Enqueue("Z");
+            Console.WriteLine(priorityQueue);
+            priorityQueue.Enqueue("C");
+            Console.WriteLine(priorityQueue);
+            priorityQueue.Enqueue("D");
+            Console.WriteLine(priorityQueue);
+            priorityQueue.Enqueue("B");
+            Console.WriteLine(priorityQueue);
+            priorityQueue.Enqueue("Q");
+            Console.WriteLine(priorityQueue);
+        }
+    }
+}
