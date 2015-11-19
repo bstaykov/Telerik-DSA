f1cb5e9e0713ded8673bbb326d732047fd1b91b6
bstaykov <boycho.staykov@gmail.com>
2015-11-19 20:42:34 +0200

    LongestCommonSubSequence 2D 3D

---
+++ 2015/DynamicProgramming/LongestCommonSubsequence/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace LongestCommonSubsequence
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var firstString = "ABCABD";
+            var secondstring = "ABABF";
+            var lcsMatrix = LongestCommonSubsequenceCalculator.DrawLongestCommonSequenceMatrix(firstString, secondstring);
+            LongestCommonSubsequenceCalculator.PrintMatrix(lcsMatrix, firstString, secondstring);
+            Console.WriteLine();
+            Console.WriteLine(LongestCommonSubsequenceCalculator.ExtractSequence(lcsMatrix, firstString, secondstring));
+
+            var thirdString = "CDAGBH";
+            var matrix3D = LongestCommonSubsequenceCalculator3D.DrawLongestCommonSequenceMatrix(firstString, secondstring, thirdString);
+            Console.WriteLine();
+            Console.WriteLine(LongestCommonSubsequenceCalculator3D.ExtractSequence(matrix3D, firstString, secondstring, thirdString));
+        }
+    }
+}
