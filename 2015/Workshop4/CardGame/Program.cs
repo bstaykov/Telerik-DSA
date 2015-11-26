0954ed9b42b4f8bdfc279d92bda36222acba9ed4
bstaykov <boycho.staykov@gmail.com>
2015-11-26 10:56:09 +0200

    CardGame tsuky

--- 2015/Workshop4/CardGame/Program.cs
+++ 2015/Workshop4/CardGame/Program.cs
@@ -2,108 +2,35 @@
 {
     using System;
     using System.Linq;
-    using System.Numerics;
 
     public class Program
     {
-        private static int n;
-        private static Node leftNode;
-
         public static void Main(string[] args)
         {
-            ReadCards();
-            CalculateMaximumPointsReachable();
-        }
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = Console.ReadLine().Split(' ')
+                .Select(int.Parse).ToArray();
 
-        private static void ReadCards()
-        {
-            n = int.Parse(Console.ReadLine());
-            var nodeValues = Console.ReadLine()
-                .Split(new char[]{ ' ' }, StringSplitOptions.RemoveEmptyEntries)
-                .Select(i => BigInteger.Parse(i))
-                .ToArray();
+            int[,] dp = new int[n, n];
 
-            leftNode = new Node(nodeValues[0]);
-            var middleNode = new Node(nodeValues[1]);
-            leftNode.Right = middleNode;
-            middleNode.Left = leftNode;
-            var rightNode = new Node(nodeValues[2]);
-            middleNode.Right = rightNode;
-            rightNode.Left = middleNode;
-
-            var lastNode = rightNode;
-
-            for (int i = 3; i < n; i++)
+            for (int length = 3; length <= n; length++)
             {
-                var newNode = new Node(nodeValues[i]);
-                lastNode.Right = newNode;
-                newNode.Left = lastNode;
-                lastNode = newNode;
-            }
-
-            //while (leftNode.Right != null)
-            //{
-            //    Console.Write(leftNode.Value + " ");
-            //    leftNode = leftNode.Right;
-            //}
-        }
-
-        private static void CalculateMaximumPointsReachable()
-        {
-            BigInteger maxPoints = 0;
-            while (n > 2)
-            {
-                var toRemoveNode = leftNode.Right;
-                var minNodeVal = toRemoveNode.Value;
-                // var max = toRemoveNode.Value * (toRemoveNode.Left.Value + toRemoveNode.Right.Value);
-                var currentNode = leftNode.Right;
-
-                if (n > 3)
+                for (int i = 0; i <= n - length; i++)
                 {
-                    while (currentNode.Right != null && currentNode.Right.Right != null)
+                    // i, i + length - 1
+                    for (int j = i + 1; j < i + length - 1; j++)
                     {
-                        // && currentNode.Right.Right != null
-                        if (currentNode.Right.Value < minNodeVal)
-                        {
-                            toRemoveNode = currentNode.Right;
-                            minNodeVal = toRemoveNode.Value;
-                        }
-                        else if (currentNode.Right.Value == minNodeVal)
-                        {
-                            var left = toRemoveNode.Value * (toRemoveNode.Left.Value + toRemoveNode.Right.Value);
-                            var right = currentNode.Right.Value * (currentNode.Value + currentNode.Right.Right.Value);
-                            if (right > left)
-                            {
-                                toRemoveNode = currentNode.Right;
-                                // currentNode = currentNode.Right;
-                            }
-                        }
+                        int current = dp[i, j] + dp[j, i + length - 1] + (numbers[j] * (numbers[i] + numbers[i + length - 1]));
 
-                        currentNode = currentNode.Right;
+                        if (dp[i, i + length - 1] < current)
+                        {
+                            dp[i, i + length - 1] = current;
+                        }
                     }
                 }
-
-                maxPoints += toRemoveNode.Value * (toRemoveNode.Left.Value + toRemoveNode.Right.Value); ;
-                toRemoveNode.Left.Right = toRemoveNode.Right;
-                toRemoveNode.Right.Left = toRemoveNode.Left;
-                n--;
             }
 
-            Console.WriteLine(maxPoints);
+            Console.WriteLine(dp[0, n - 1]);
         }
-    }
-
-    public class Node
-    {
-        public Node(BigInteger value)
-        {
-            this.Value = value;
-        }
-
-        public BigInteger Value { get; set; }
-
-        public Node Left { get; set; }
-
-        public Node Right { get; set; }
     }
 }
