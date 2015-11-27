9c3b04afd97113fde3d5019cd75b6c26a1321a46
bstaykov <boycho.staykov@gmail.com>
2015-11-27 11:07:44 +0200

    Search Algorithms Hints

---
+++ 2015/StringAlgorithms/RabinKarpSearch/StartUp.cs
@@ -0,0 +1,42 @@
+﻿namespace RabinKarpSearch
+{
+    using System;    
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string text = "dwadfrg fefe abc123456 e  da abc123456 e dawd abc123456 efrfgr";
+            string pattern = "abc123456";
+
+            int textLength = text.Length;
+            int patternLength = pattern.Length;
+
+            if (patternLength > textLength)
+            {
+                return;
+            }
+
+            Hash.ComputePowers(patternLength);
+
+            Hash hpattern = new Hash(pattern);
+            Hash hwindow = new Hash(text.Substring(0, patternLength));
+
+            if (hpattern.Value == hwindow.Value)
+            {
+                Console.WriteLine("Math at 0");
+            }
+
+            for (int i = 1; i <= textLength - patternLength; i++)
+            {
+                hwindow.Add(text[i + patternLength - 1]);
+                hwindow.Remove(text[i - 1], patternLength);
+
+                if (hpattern.Value == hwindow.Value)
+                {
+                    Console.WriteLine("Math at {0}", i);
+                }
+            }
+        }
+    }
+}
