eb22e80c475ecc873bbb600c1d72401b1d850c91
bstaykov <boycho.staykov@gmail.com>
2015-11-28 18:55:41 +0200

    problem not resolved exception!

--- 2015/Workshop5StringsAndGready/Source/Program.cs
+++ 2015/Workshop5StringsAndGready/Source/Program.cs
@@ -1,9 +1,156 @@
 ﻿namespace Source
 {
+    using System;
+    using System.Collections.Generic;
+
     public class Program
     {
+        private static char[] text;
+        private static char[] pattern;
+        private static List<int> offsets = new List<int>();
+        private static int offsetsSum = 0;
+        private static int lastOffsetIndex = 0;
+        private static int pmatches = 0;
+
         public static void Main(string[] args)
         {
+            ReadInput();
+            CountPMatches();
+            Console.WriteLine(pmatches);
+            Console.WriteLine(offsetsSum);
+
+            // Console.WriteLine(string.Join(" ", offsets));
+        }
+
+        private static void CountPMatches()
+        {
+            int patternFirstTokenIndex = FindPatternFirstTokenIndex();
+            if (patternFirstTokenIndex != -1)
+            {
+                int textTokenIndex;
+                int startIndex = 0;
+                do
+                {
+                    textTokenIndex = FindTextTokenIndex(pattern[patternFirstTokenIndex], startIndex);
+                    if (textTokenIndex != -1)
+                    {
+                        if (IsPMatch(textTokenIndex, patternFirstTokenIndex))
+                        {
+                            pmatches += 1;
+                            var offset = CalculateOffset(textTokenIndex, patternFirstTokenIndex);
+
+                            // offsets.Add(offset + 1);
+                            offsetsSum += offset + 1;
+                            lastOffsetIndex = textTokenIndex - patternFirstTokenIndex + pattern.Length;
+                            startIndex = lastOffsetIndex;
+                        }
+                    }
+                } 
+                while (textTokenIndex != -1);
+            }
+            else
+            {
+                // big mess
+            }
+        }
+
+        private static int CalculateOffset(int textTokenIndex, int patternFirstTokenIndex)
+        {
+            var lastTextIndex = textTokenIndex - patternFirstTokenIndex;
+            var offset = lastTextIndex - lastOffsetIndex;
+            return offset;
+        }
+
+        private static bool IsPMatch(int textTokenIndex, int patternTokenIndex)
+        {
+            if (textTokenIndex < patternTokenIndex)
+            {
+                return false;
+            }
+
+            if (textTokenIndex + pattern.Length - patternTokenIndex > text.Length)
+            {
+                return false;
+            }
+
+            var corespondingMatches = new Dictionary<char, char>();
+            var startIndex = textTokenIndex - patternTokenIndex;
+            var patternIndex = -1;
+            for (int i = startIndex; i < startIndex + pattern.Length; i++)
+            {
+                patternIndex++;
+                var textChar = text[i];
+                var patternChar = pattern[patternIndex];
+                bool isTextCharUpper = char.IsUpper(textChar);
+                bool isPatternCharUpper = char.IsUpper(patternChar);
+                if (isTextCharUpper && isPatternCharUpper)
+                {
+                    if (textChar == patternChar)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                } 
+                else if (isTextCharUpper || isPatternCharUpper)
+                {
+                    return false;
+                }
+                else
+                {
+                    if (corespondingMatches.ContainsKey(patternChar))
+                    {
+                        if (corespondingMatches[patternChar] == textChar)
+                        {
+                            continue;
+                        } 
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        corespondingMatches.Add(patternChar, textChar);
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        private static int FindTextTokenIndex(char token, int startIndex)
+        {
+            for (int i = startIndex; i < text.Length; i++)
+            {
+                if (text[i] == token)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        private static int FindPatternFirstTokenIndex()
+        {
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                if (char.IsUpper(pattern[i]))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        private static void ReadInput()
+        {
+            pattern = Console.ReadLine().ToCharArray();
+            text = Console.ReadLine().ToCharArray();
         }
     }
 }
