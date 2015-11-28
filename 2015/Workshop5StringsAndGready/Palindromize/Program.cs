3ca15aa3533cec0ba9a7e940a0befa5fcd7d60a3
bstaykov <boycho.staykov@gmail.com>
2015-11-28 12:20:17 +0200

    Palindromes Best

--- 2015/Workshop5StringsAndGready/Palindromize/Program.cs
+++ 2015/Workshop5StringsAndGready/Palindromize/Program.cs
@@ -5,7 +5,51 @@
 
     public class Program
     {
+        private static char[] inputAsChars;
+
         public static void Main(string[] args)
+        {
+            // FindPalindromeWithSubstrings();
+            FindPalindromeWithCharArray();
+        }
+
+        private static void FindPalindromeWithCharArray()
+        {
+            inputAsChars = Console.ReadLine().ToCharArray();
+            if (AreOrderedAsPalindroms())
+            {
+                Console.WriteLine(inputAsChars);
+                return;
+            }
+            else
+            {
+                for (int i = 1; i < inputAsChars.Length - 1; i++)
+                {
+                    if (IsPalindrom(i))
+                    {
+                        PrintPalindrom(i);
+                        return;
+                    }
+                }
+
+                PrintPalindrom(inputAsChars.Length - 1);
+            }
+        }
+
+        private static void PrintPalindrom(int length)
+        {
+            for (int i = 0; i < inputAsChars.Length; i++)
+            {
+                Console.Write(inputAsChars[i]);
+            }
+
+            for (int i = length - 1; i >= 0; i--)
+            {
+                Console.Write(inputAsChars[i]);
+            }
+        }
+
+        private static void FindPalindromeWithSubstrings()
         {
             string input = Console.ReadLine();
             string reversed = string.Join(string.Empty, input.Reverse());
@@ -25,6 +69,36 @@
                     return;
                 }
             }
+        }
+
+        private static bool IsPalindrom(int length)
+        {
+            for (int i = length; i < (inputAsChars.Length + length) / 2; i++)
+            {
+                var a = inputAsChars[i];
+                if (length - i > 0)
+                {
+                }
+                else if (inputAsChars[i] != inputAsChars[inputAsChars.Length - i + length - 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static bool AreOrderedAsPalindroms()
+        {
+            for (int i = 0; i < inputAsChars.Length / 2; i++)
+            {
+                if (inputAsChars[i] != inputAsChars[inputAsChars.Length - 1 - i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
 
         private static bool IsPalindrom(string palindromeCandidate)
