e809f586505092e3f89e557e1a7d6bf16898787f
bstaykov <boycho.staykov@gmail.com>
2015-11-28 10:26:28 +0200

    Palindromes

--- 2015/Workshop5StringsAndGready/Palindromize/Program.cs
+++ 2015/Workshop5StringsAndGready/Palindromize/Program.cs
@@ -1,9 +1,43 @@
 ﻿namespace Palindromize
 {
+    using System;
+    using System.Linq;
+
     public class Program
     {
         public static void Main(string[] args)
         {
+            string input = Console.ReadLine();
+            string reversed = string.Join(string.Empty, input.Reverse());
+
+            if (IsPalindrom(input))
+            {
+                Console.WriteLine(input);
+                return;
+            }
+
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                var palindromeCandidate = input + reversed.Substring(input.Length - 1 - i, 1 + i);
+                if (IsPalindrom(palindromeCandidate))
+                {
+                    Console.WriteLine(palindromeCandidate);
+                    return;
+                }
+            }
+        }
+
+        private static bool IsPalindrom(string palindromeCandidate)
+        {
+            for (int i = 0; i < palindromeCandidate.Length / 2; i++)
+            {
+                if (palindromeCandidate[i] != palindromeCandidate[palindromeCandidate.Length - 1 - i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
